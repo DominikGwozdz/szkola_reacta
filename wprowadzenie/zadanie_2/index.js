81458c0ff7a49cc6dad28204397b4d5cace95633
Dominik <dwgwozdz@gmail.com>
2022-06-04 16:06:12 +0200

    add zadanie_2 solution for wprowadzenie module

---
+++ wprowadzenie/zadanie_2/index.js
@@ -0,0 +1,38 @@
+const prompt = require('prompt');
+
+function getNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+const randomNumber = getNumber();
+
+const schema = {
+    properties: {
+        yourNumber: {
+            description: 'Podaj cyfre od 1 do 10',
+            pattern: /^\d+$/,
+            message: 'Spróbuj ponownie',
+            required: true,
+            type: 'integer',
+            conform: function (value) {
+                return value === randomNumber;
+            }
+        }
+    }
+}
+
+prompt.start();
+
+prompt.get(schema, function (err, result) {
+    console.log("Udało się");
+});
+
+
+
+
+
+
+
+
+// console.log(`Udało się`);
+// console.log(`Spróbuj ponownie`);
