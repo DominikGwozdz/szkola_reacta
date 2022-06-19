9a708ccbe9cc284e388b870723b7600855c9fad3
Dominik <dwgwozdz@gmail.com>
2022-06-19 16:31:01 +0200

    base for components tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
+import React, {useState} from "react";
 import './App.css';
 
+// class News extends React.Component {
+//     state = {
+//         date: new Date(),
+//         score: 5
+//     };
+//
+//     componentDidMount() {
+//         let loading = true;
+//         this.setState({score: 7}, () => {
+//             loading = false;
+//         });
+//
+//     }
+//
+//     render() {
+//         const {header, intro} = this.props;
+//         const {score} = this.state;
+//         return (
+//             <div className="News">
+//                 <h2>{header}</h2>
+//                 <p>{intro}</p>
+//                 <p>{score}</p>
+//             </div>
+//         );
+//     }
+// }
+
+
+function News({ header, intro }) {
+    const [score, setScore] = useState(5);
+
+    return (
+        <div className="News">
+            <h2>{header}</h2>
+            <p>{intro}</p>
+            <p>{score}</p>
+        </div>
+    );
+}
+
+const news = [
+    {header: 'Nagłówek 1', intro: 'Intro 1'},
+    {header: 'Nagłówek 2: Breaking news', intro: 'Intro 2'},
+    {header: 'Nagłówek 3: Nie śpie bo coś tam', intro: 'Intro 3'}
+];
+
 function App() {
-  return (
-      <div className="App">
-        <p>Hello!</p>
-        <p>Szkoło Reacta</p>
-        <a href="/" className="App-link">Learn react</a>
-      </div>
-  );
+    return (
+        <div>
+            {news.map((elem, index) => (
+                <News key={`news-${index}`} header={elem.header} intro={elem.intro} />
+            ))}
+        </div>
+    )
 }
 
 export default App;
